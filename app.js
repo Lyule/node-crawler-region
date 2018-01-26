9bef23892dc6d5a36e8a19e0bf30fcaa5530649c
[lyule] <[857277886@qq.com]>
2018-01-27 01:16:21 +0800

    remove comment

--- app.js
+++ app.js
@@ -1,35 +1,31 @@
 const Crawler = require('crawler');
 const temme = require('temme').default;
-const {defineFilter} = require('temme');
 const _ = require('lodash');
 const fs = require('fs');
 
-// defineFilter('replace', (arg1, arg2) => {
-//     console.log(arg1)
-//     console.log(arg2)
-// })
 var c = new Crawler({
     maxConnections: 100,
     // This will be called for each crawled page
     callback: function (error, res, done) {
         if (error) {
-            console.log(error);
-        } else {
+            return console.log(error);
+        }else{
             let json = _.compact(temme(res.body, `[class] td@{a[href=$url]{$text}};`));
 
-            fs.appendFileSync('region.json', JSON.stringify(json))
-            _.forEach(json, item => {
-                getRegion(item.url)
-            });
+            fs.appendFileSync('region.json', JSON.stringify(json));
+
+            _.forEach(json, item => getRegion(item.url));
         }
+
         done();
     }
-    
-});
-const baseUrl = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/';
 
-c.queue(`${baseUrl}index.html`);
+});
+
+getRegion('index.html');
 
 function getRegion(url) {
+    const baseUrl = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/';
+
     c.queue(`${baseUrl}${url}`);
 }
